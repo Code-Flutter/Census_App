d8adc5e73e8d21e9322de7886e12220668acc8bc
allankalema <allankalema4@gmail.com>
2023-08-09 13:09:37 +0300

    working with play.dart

--- lib/play.dart
+++ lib/play.dart
@@ -9,16 +9,29 @@ class Play extends StatelessWidget {
     DatabaseService householdData = DatabaseService();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Play'),
+        title: const Text('HouseHolds regestered'),
       ),
       body: Center(
         child: Column(
           children: [
-            ElevatedButton(
-                onPressed: () async {
-                  await householdData.fetchData();
-                },
-                child: Text('Get Data')),
+            Card(
+              color: Colors.grey[100], // Off-white color
+              elevation: 0, // No shadow
+              margin: EdgeInsets.all(0), // No margin
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Testing',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: 16), // Add some spacing
+            // ElevatedButton(
+            //     onPressed: () async {
+            //       await householdData.fetchData();
+            //     },
+            //     child: Text('Get Data')),
             Text('Welcome to the Play Screen!'),
           ],
         ),
