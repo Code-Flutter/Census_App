1e9c0cc78319e9d0a18d653239f81bc186116ec7
fearless_programmer <trevorssemwanga@gmail.com>
2023-08-10 10:04:39 +0300

    Added HouseHold Cards
    These cards will hold the Household information enetered by a specific Census Collector

--- lib/play.dart
+++ lib/play.dart
@@ -6,34 +6,116 @@ class Play extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    DatabaseService householdData = DatabaseService();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HouseHolds regestered'),
+        title: const Text('HouseHolds registered'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Card(
-              color: Colors.grey[100], // Off-white color
-              elevation: 0, // No shadow
-              margin: EdgeInsets.all(0), // No margin
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Testing',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            SizedBox(height: 16), // Add some spacing
-            // ElevatedButton(
-            //     onPressed: () async {
-            //       await householdData.fetchData();
-            //     },
-            //     child: Text('Get Data')),
-            Text('Welcome to the Play Screen!'),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: FutureBuilder(
+          future: DatabaseService().fetchData(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> householdData) {
+            if (householdData.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator(); // Display loading indicator while waiting
+            }
+
+            if (householdData.hasError) {
+              return Text('Error: ${householdData.error}');
+            }
+
+            if (!householdData.hasData) {
+              return Text('No data available');
+            }
+
+            List<Map<String, dynamic>>? householdRecords = householdData.data;
+
+            return ListView.builder(
+              itemCount: householdRecords?.length,
+              itemBuilder: (BuildContext context, int index) {
+                Map<String, dynamic> record = householdRecords![index];
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Card(
+                      color: Colors.amberAccent[100], // Off-white color
+                      elevation: 1, // No shadow
+                      margin: EdgeInsets.all(0), // No margin
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('HouseHold ID',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black)),
+                            Text(
+                              '${record["HouseHold ID"]}',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(height: 12),
+                            Text('HouseHold Head',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black)),
+                            Text(
+                              '${record["Head of HouseHold"]}',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(height: 12),
+                            Text('Village',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black)),
+                            Text(
+                              '${record["Village"]}',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.edit,
+                                    size: 25.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.delete,
+                                    size: 25.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20.0)
+                  ],
+                );
+              },
+            );
+          },
         ),
       ),
     );
